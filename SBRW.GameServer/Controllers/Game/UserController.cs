3a19f30f7da6f20bf2c429cdfa304fb55b464a59
LeoCodes21 <coderleo42+leocodes@gmail.com>
2019-11-28 12:51:46 -0500

    UserController WIP

--- SBRW.GameServer/Controllers/Game/UserController.cs
+++ SBRW.GameServer/Controllers/Game/UserController.cs
@@ -1,13 +1,31 @@
-﻿using System.Net.Mime;
+﻿using System.Collections.Generic;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
+using Victory.DataLayer.Serialization;
+using Victory.TransferObjects.User;
 
 namespace SBRW.GameServer.Controllers.Game
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Consumes(MediaTypeNames.Application.Xml)]
     [Produces(MediaTypeNames.Application.Xml)]
     public class UserController : ControllerBase
     {
-        
+        /// <summary>
+        /// Establishes a permanent gameplay session for a client.
+        /// </summary>
+        /// <param name="data">Identifying information sent by the client.</param>
+        /// <returns>A <see cref="UserInfo"/> object with session details.</returns>
+        [HttpPost("GetPermanentSession")]
+        public UserInfo GetPermanentSession([FromBody] GetPermanentSessionData data)
+        {
+            return new UserInfo
+            {
+                defaultPersonaIdx = 0,
+                personas = new List<ProfileData>(),
+                user = new User()
+            };
+        }
     }
 }
